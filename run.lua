0fa6dfefc4aa65430e5ba2b07dbec4f3951fa00c
BaoBao-creator <ba0ba0113377@gmail.com>
2025-08-19 11:03:14 +0700

    Update run.lua

--- run.lua
+++ run.lua
@@ -1,3 +1,7 @@
+local mainfarm = workspace:WaitForChild("Farm")
+local userfarm = nil
+for _, farm in ipairs(mainfarm) do
+  if
 local function shovelplant(name)
   game:GetService("ReplicatedStorage").GameEvents.Remove_Item:FireServer(workspace.Farm.Farm.Important.Plants_Physical.Cauliflower["1"])
 end
